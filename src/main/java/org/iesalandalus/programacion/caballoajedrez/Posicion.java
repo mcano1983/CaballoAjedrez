9ea52e56ac82dd0967929d4f911e5697c1fd4c37
MiguelAngel <mcano.1983@gmail.com>
2018-11-16 20:57:15 +0100

    Creacion metodos Set y Get.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -4,4 +4,25 @@ public class Posicion {
  private int fila;
  private char columna;
  
+ public int getFila() {
+	 return this.fila;
+ }
+ public char getColumna() {
+	 return this.columna;
+ }
+ public int setFila(int fila) {
+	 if(fila>=1 && fila<=8) {
+		 this.fila = fila;
+	 }else {
+		 throw new IllegalArgumentException(); 
+	 }
+	 return fila;
+	 
+ }
+ public int setColumna(char columna) {
+	 if(columna=='a' || columna=='b' || columna=='c' || columna=='d' || columna=='e' || columna=='f'|| columna=='g'|| columna=='h');
+	 this.columna = columna;
+	 return columna;
+ }
+
 }
