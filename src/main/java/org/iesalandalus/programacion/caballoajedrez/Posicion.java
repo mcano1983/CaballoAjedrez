adc268b1557f3c9ea00cc2aabccf77612042606c
MiguelAngel <mcano.1983@gmail.com>
2018-11-21 09:26:42 +0100

    Creacion del constructor copia,metodo equals y toString clase Posicion

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.caballoajedrez;
 
+import static org.junit.Assert.assertEquals;
+
 public class Posicion {
 	private int fila;
 	private char columna;
@@ -16,7 +18,7 @@ public class Posicion {
 		if (fila >= 1 && fila <= 8) {
 			this.fila = fila;
 		} else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		}
 
 	}
@@ -26,7 +28,7 @@ public class Posicion {
 				|| columna == 'g' || columna == 'h') {
 			this.columna = columna;
 		} else {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("ERROR: Columna no válida.");
 		}
 
 	}
@@ -35,4 +37,44 @@ public class Posicion {
 		this.setFila(fila);
 		this.setColumna(columna);
 	}
+	public Posicion (Posicion nuevaPosicion) { 
+		if (nuevaPosicion!=null) {
+		this.fila=nuevaPosicion.getFila();
+		this.columna=nuevaPosicion.getColumna();
+		}else {
+			throw new IllegalArgumentException("ERROR: No es posible copiar una posición nula.");	
+		}
+		
+		}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + columna;
+		result = prime * result + fila;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (columna != other.columna)
+			return false;
+		if (fila != other.fila)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[fila=" + this.fila + ", columna=" + this.columna + "]";
+	}
+	
+	
 }
