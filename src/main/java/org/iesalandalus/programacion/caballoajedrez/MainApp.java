5ac2f2504775d5a66aa8955fbb172353a2b1dc46
MiguelAngel <mcano.1983@gmail.com>
2018-11-22 11:24:08 +0100

    Subida Al repositiorio.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/MainApp.java
@@ -9,23 +9,6 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("Programa para aprender a colocar y mover un caballo en el tablero de ajedrez");
 
-		Caballo caballo = new Caballo(Color.NEGRO);
-		
-		System.out.println(caballo.getPosicion());
-		try {
-			caballo.mover(Direccion.ABAJO_DERECHA);
-		}catch(OperationNotSupportedException e) {}
-		
-		System.out.println(caballo.getPosicion());
-		try {
-			caballo.mover(Direccion.ABAJO_DERECHA);
-		}catch(OperationNotSupportedException e) {}
-		
-		System.out.println(caballo.getPosicion());
-		try {
-			caballo.mover(Direccion.ABAJO_DERECHA);
-		}catch(OperationNotSupportedException e) {}
-		
-		System.out.println(caballo.getPosicion());
+	
 	}
 }
